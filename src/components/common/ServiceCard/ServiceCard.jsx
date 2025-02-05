99cd4e96ed23be15d32b1bb807e43efdf57f27e3
Usama1190 <usamaisrar1190@gmail.com>
2025-02-05 21:38:11 +0500

    Keep working on services card images on Feb 05, 25

--- src/components/common/ServiceCard/ServiceCard.jsx
+++ src/components/common/ServiceCard/ServiceCard.jsx
@@ -1,13 +1,15 @@
-import ImageCom from '../../Image/ImageCom';
+import CardImage from '../../CardImage/cardImage';
 import styles from './ServiceCard.module.css';
 
 const ServiceCard = ({ data }) => {
-  const { title, imgUrl, description, imgAlt, btnText, btnLink } = data;
+  const { title, imgUrl, description, imgAlt, btnText, btnLink, id, phd, phl } = data;
   return (
     <div className={styles.scwp}>
       <h3 className={styles.sct}>{title}</h3>
       {/* <img className={styles.scig} src={imgUrl} alt={imgAlt} /> */}
-      <ImageCom svgUrl={imgUrl} pathDark={''} pathLight={''} />
+      <div className={styles.cw}>
+        <CardImage phd={phd} phl={phl} id={id} />
+      </div>
       <p className={styles.scdes}>{description}</p>
       <a href={btnLink}><b>{btnText}</b></a>
     </div>
--- src/utils/constant/serviceCardData.js
+++ src/utils/constant/serviceCardData.js
@@ -1,54 +1,72 @@
 const serviceCardData = {
   en: [
     {
+      id: 1,
       title: "Payment Done",
       description: "Pay with a Visa or PayPal card and without much ado.",
       imgUrl: "./assets/imgs/cardImage1.svg",
       imgAlt: "order payment image",
       btnText: "Learn more",
       btnLink: "#",
+      phd: [38, 52, 53, 54, 55, 58, 59, 60, 62, 63, 64, 65, 66, 69, 77, 78, 81, 83],
+      phl: [1, 67]
     },
     {
+      id: 2,
       title: "Find Your Product",
       description: "We offer sale of products through the Internet.",
       imgUrl: "./assets/imgs/cardImage2.svg",
       imgAlt: "digital product image",
       btnText: "Learn more",
       btnLink: "#",
+      phd: [1],
+      phl: [2]
     },
     {
+      id: 3,
       title: "Product Received",
       description: "In our app you can see the delay time of your order.",
       imgUrl: "./assets/imgs/cardImage3.svg",
       imgAlt: "digital product received",
       btnText: "Learn more",
       btnLink: "#",
+      phd: [1],
+      phl: [2]
     },
   ],
   ur: [
     {
+      id: 1,
       title: "ادائیگی ہو گئی۔",
       description: "ویزا یا پے پال کارڈ کے ساتھ اور زیادہ ایڈو کے بغیر ادائیگی کریں۔",
       imgUrl: "./assets/imgs/cardImage1.svg",
       imgAlt: "order payment image",
       btnText: "مزید جانیں",
       btnLink: "#",
+      phd: [1],
+      phl: [2]
     },
     {
+      id: 2,
       title: "اپنی پروڈکٹ تلاش کریں۔",
       description: "ہم انٹرنیٹ کے ذریعے مصنوعات کی فروخت پیش کرتے ہیں۔",
       imgUrl: "./assets/imgs/cardImage2.svg",
       imgAlt: "digital product image",
       btnText: "مزید جانیں",
       btnLink: "#",
+      phd: [1],
+      phl: [2]
     },
     {
+      id: 3,
       title: "پروڈکٹ موصول ہوا۔",
       description: "ہماری ایپ میں آپ اپنے آرڈر میں تاخیر کا وقت دیکھ سکتے ہیں۔",
       imgUrl: "./assets/imgs/cardImage3.svg",
       imgAlt: "digital product received",
       btnText: "مزید جانیں",
       btnLink: "#",
+      phd: [1],
+      phl: [2]
     },
   ],
 };
