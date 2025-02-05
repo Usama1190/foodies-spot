37d9029281dd2bee445f03e28b4480a32fcc7672
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2025-02-05 19:20:58 +0500

    Hero images work successfully! on Feb 05, 25

--- src/components/Image/ImageCom.jsx
+++ src/components/Image/ImageCom.jsx
@@ -1,28 +1,156 @@
-'use client'; 
+"use client";
 
-import BannerImage from "../../assets/imgs/bannerImage.svg?react";
+import BannerImageOne from "../../assets/imgs/bannerImage.svg?react";
+import BannerImageTwo from "../../assets/imgs/bannerImage2.svg?react";
+import BannerImageThree from "../../assets/imgs/bannerImage3.svg?react";
+import BannerImageFour from "../../assets/imgs/bannerImage4.svg?react";
+
 import { useColor } from "../../context/colorContext";
 
-const ImageCom = ({ svgUrl, pathDark, pathLightl }) => {
+const ImageCom = ({ svgUrl, pathDark, pathLight }) => {
   const { selectedColor } = useColor();
-  console.log(svgUrl);
-  return (
-    <>
-      <BannerImage className="custom-svg" />
-      {
-        pathDark?.map((item, index) => {
-          return (
-            <style key={index}>
-            {`
-              .custom-svg path:nth-child(${item}) { fill: ${selectedColor.darkColor}; },
-            `}
-          </style>
-          )
-        })
-      }
-    </>
-  );
-};
 
+  {
+    if (svgUrl === "../../assets/imgs/bannerImage.svg?react") {
+      return (
+        <>
+          <BannerImageOne className="custom-svg1" />
+          {
+            pathDark?.map((item, index) => {
+              return (
+                <style key={index}>
+                  {`
+                    .custom-svg1 path:nth-child(${item}) { 
+                        fill: ${selectedColor.darkColor};
+                      },
+                  `}
+                </style>
+              );
+            })
+          }
+
+          {
+            pathLight?.map((item, index) => {
+              return (
+                <style key={index}>
+                  {`
+                    .custom-svg1 path:nth-child(${item}) { 
+                        fill: ${selectedColor.lightColor};
+                      },
+                  `}
+                </style>
+              );
+            })
+          }
+        </>
+      );
+    } 
+    else if (svgUrl === "../../assets/imgs/bannerImage2.svg?react") {
+      return (
+        <>
+          <BannerImageTwo className="custom-svg2" />
+
+          {
+            pathDark?.map((item, index) => {
+              return (
+                <style key={index}>
+                  {`
+                    .custom-svg2 path:nth-child(${item}) { 
+                        fill: ${selectedColor.darkColor};
+                      },
+                  `}
+                </style>
+              );
+            })
+          }
+
+          {
+            pathLight?.map((item, index) => {
+              return (
+                <style key={index}>
+                  {`
+                    .custom-svg2 path:nth-child(${item}) { 
+                        fill: ${selectedColor.lightColor};
+                      },
+                  `}
+                </style>
+              );
+            })
+          }
+        </>
+      );
+    } 
+    else if (svgUrl === "../../assets/imgs/bannerImage3.svg?react") {
+      return (
+        <>
+          <BannerImageThree className="custom-svg3" />
+
+          {
+            pathDark?.map((item, index) => {
+              return (
+                <style key={index}>
+                  {`
+                    .custom-svg3 path:nth-child(${item}) { 
+                        fill: ${selectedColor.darkColor};
+                      },
+                  `}
+                </style>
+              );
+            })
+          }
+
+          {
+            pathLight?.map((item, index) => {
+              return (
+                <style key={index}>
+                  {`
+                    .custom-svg3 path:nth-child(${item}) { 
+                        fill: ${selectedColor.lightColor};
+                      },
+                  `}
+                </style>
+              );
+            })
+          }
+        </>
+      );
+    } 
+    else if (svgUrl === "../../assets/imgs/bannerImage4.svg?react") {
+      return (
+        <>
+          <BannerImageFour className="custom-svg4" />
+
+          {
+            pathDark?.map((item, index) => {
+              return (
+                <style key={index}>
+                  {`
+                    .custom-svg4 path:nth-child(${item}) { 
+                        fill: ${selectedColor.darkColor};
+                      },
+                  `}
+                </style>
+              );
+            })
+          }
+
+          {
+            pathLight?.map((item, index) => {
+              return (
+                <style key={index}>
+                  {`
+                    .custom-svg4 path:nth-child(${item}) { 
+                        fill: ${selectedColor.lightColor};
+                      },
+                  `}
+                </style>
+              );
+            })
+          }
+        </>
+      );
+    }
+  }
+};
 
 export default ImageCom;
--- src/utils/constant/heroData.js
+++ src/utils/constant/heroData.js
@@ -7,7 +7,7 @@ const heroData = {
         "Order your favorite foods at any time and we will deliver them right to where you are.",
       btnText: ["Get Started"],
       btnLink: ["/"],
-      imgUrl: "./assets/imgs/bannerImage.svg",
+      imgUrl: "../../assets/imgs/bannerImage.svg?react",
       imgAlt: "food delivery rider image",
       imgLaout: { width: "100%", height: "19.75rem" },
       pd: [20, 33, 34, 35, 36, 37, 39, 40, 42, 44, 46, 56, 57, 64, 71, 75, 79, 85, 86, 87, 88, 101, 102, 109, 110, 112, 113, 114],
@@ -18,22 +18,22 @@ const heroData = {
       title: "Find Out A Little More About Us",
       description:
         "We are a company dedicated to the distribution of products by delivery to your home or to the place where you are, with the best quality of delivery.",
-      imgUrl: "./assets/imgs/bannerImage2.svg",
+      imgUrl: "../../assets/imgs/bannerImage2.svg?react",
       imgAlt: "delivery boy on the door",
       isrotate: true,
       imgLaout: { width: "100%", height: "22.1875rem" },
-      // pd: [26, 29, 32, 42, 43, 53, 64, 65, 75, 80, 85],
-      // pl: [1]
+      pd: [26, 29, 32, 42, 43, 53, 64, 65, 75, 80, 85],
+      pl: [1]
     },
     {
       title: "Your Safety Is Important",
       description:
         "When your order reaches you, we have the health safety protocols, so that you are protected from any disease. Watch the video of how the delivery is made.",
-      imgUrl: "./assets/imgs/bannerImage3.svg",
+      imgUrl: "../../assets/imgs/bannerImage3.svg?react",
       imgAlt: "delivery boy deliver order",
       imgLaout: { width: "100%", height: "20.8125rem" },
-      // pd: [7, 10, 23, 24, 32, 33, 35, 36, 45, 46, 47, 52, 57, 58, 61, 67, 86, 88],
-      // pl: [1]
+      pd: [7, 10, 23, 24, 32, 33, 35, 36, 45, 46, 47, 52, 57, 58, 61, 67, 86, 88],
+      pl: [1]
     },
     {
       title: "Watch Your Delivery At Any Time",
@@ -42,12 +42,12 @@ const heroData = {
       btnText: ["App Store", "Google Play"],
       btnLink: ["/", "/"],
       btnIcons: ["fa fa-apple", "fa fa-play"],
-      imgUrl: "./assets/imgs/bannerImage4.svg",
+      imgUrl: "../../assets/imgs/bannerImage4.svg?react",
       imgAlt: "delivery boy set location",
       isrotate: true,
       imgLaout: { width: "100%", height: "24.125rem" },
-      // pd: [16, 32, 33, 73, 105, 115, 116, 117],
-      // pl: [1, 29, 107]
+      pd: [16, 32, 33, 73, 105, 115, 116, 117],
+      pl: [1, 29, 107]
     },
   ],
   ur: [
@@ -58,7 +58,7 @@ const heroData = {
         "کسی بھی وقت اپنے پسندیدہ کھانے کا آرڈر دیں اور ہم انہیں آپ کی جگہ پر پہنچائیں گے۔",
       btnText: ["شروع کریں"],
       btnLink: ["/"],
-      imgUrl: "./assets/imgs/bannerImage.svg",
+      imgUrl: "../../assets/imgs/bannerImage.svg?react",
       imgAlt: "food delivery rider image",
       imgLaout: { width: "100%", height: "19.75rem" },
       pd: [20, 33, 34, 35, 36, 37, 39, 40, 42, 44, 46, 56, 57, 64, 71, 75, 79, 85, 86, 87, 88, 101, 102, 109, 110, 112, 113, 114],
@@ -69,22 +69,22 @@ const heroData = {
       title: "ہمارے بارے میں تھوڑا سا مزید جانیں۔",
       description:
         "ہم ایک ایسی کمپنی ہیں جو آپ کے گھر یا اس جگہ پر جہاں آپ ہیں، ترسیل کے بہترین معیار کے ساتھ مصنوعات کی تقسیم کے لیے وقف ہیں",
-      imgUrl: "./assets/imgs/bannerImage2.svg",
+      imgUrl: "../../assets/imgs/bannerImage2.svg?react",
       imgAlt: "delivery boy on the door",
       isrotate: true,
       imgLaout: { width: "100%", height: "22.1875rem" },
-      // pd: [26, 29, 32, 42, 43, 53, 64, 65, 75, 80, 85],
-      // pl: [1]
+      pd: [26, 29, 32, 42, 43, 53, 64, 65, 75, 80, 85],
+      pl: [1]
     },
     {
       title: "آپ کی حفاظت اہم ہے۔",
       description:
         "جب آپ کا آرڈر آپ تک پہنچتا ہے، تو ہمارے پاس ہیلتھ سیفٹی پروٹوکول ہوتے ہیں، تاکہ آپ کسی بھی بیماری سے محفوظ رہیں۔ ڈیلیوری کیسے ہوتی ہے ویڈیو دیکھیں۔",
-      imgUrl: "./assets/imgs/bannerImage3.svg",
+      imgUrl: "../../assets/imgs/bannerImage3.svg?react",
       imgAlt: "delivery boy deliver order",
       imgLaout: { width: "100%", height: "20.8125rem" },
-      // pd: [7, 10, 23, 24, 32, 33, 35, 36, 45, 46, 47, 52, 57, 58, 61, 67, 86, 88],
-      // pl: [1]
+      pd: [7, 10, 23, 24, 32, 33, 35, 36, 45, 46, 47, 52, 57, 58, 61, 67, 86, 88],
+      pl: [1]
     },
     {
       title: "کسی بھی وقت اپنی ڈیلیوری دیکھیں",
@@ -93,12 +93,12 @@ const heroData = {
       btnText: ["App Store", "Google Play"],
       btnLink: ["/", "/"],
       btnIcons: ["fa fa-apple", "fa fa-play"],
-      imgUrl: "./assets/imgs/bannerImage4.svg",
+      imgUrl: "../../assets/imgs/bannerImage4.svg?react",
       imgAlt: "delivery boy set location",
       isrotate: true,
       imgLaout: { width: "100%", height: "24.125rem" },
-      // pd: [16, 32, 33, 73, 105, 115, 116, 117],
-      // pl: [1, 29, 107]
+      pd: [16, 32, 33, 73, 105, 115, 116, 117],
+      pl: [1, 29, 107]
     },
   ],
 };
